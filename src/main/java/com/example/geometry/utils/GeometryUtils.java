c44b701618dd1dd87c047a68fbb5d0a6780618c3
ivan <belyakov_ivan96@mail.ru>
2024-07-22 09:16:28 +0300

    fix 1.1.0-SNAPSHOT fix version 1.2.0

--- src/main/java/com/example/geometry/utils/GeometryUtils.java
+++
@@ -1,7 +0,0 @@
-package com.example.geometry.utils;
-
-public class GeometryUtils {
-    public static double convertSquareMetersToSquareFeet(double squareMeters) {
-        return squareMeters * 10.7639;
-    }
-}
