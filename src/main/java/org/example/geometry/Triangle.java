b47ec7e2d1bbea3ccef46105d1833a90ce473461
ivan <belyakov_ivan96@mail.ru>
2024-07-22 08:28:43 +0300

    init

---
+++ src/main/java/org/example/geometry/Triangle.java
@@ -0,0 +1,20 @@
+package org.example.geometry;
+
+public class Triangle {
+    private double a, b, c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getArea() {
+        double s = (a + b + c) / 2; // полупериметр
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    public double getPerimeter() {
+        return a + b + c;
+    }
+}
